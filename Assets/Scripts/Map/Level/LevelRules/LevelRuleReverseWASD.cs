aaffdad29a6aa3c5b4d9c1bbe86da5071693ea3c
Maxyjof <3478124403@qq.com>
2026-07-03 21:36:35 +0800

    调整反向WASD规则的实现方式，并配置地图

--- Assets/Scripts/Map/Level/LevelRules/LevelRuleReverseWASD.cs
+++ Assets/Scripts/Map/Level/LevelRules/LevelRuleReverseWASD.cs
@@ -8,18 +8,33 @@ namespace Game.Map
 {
     public class LevelRuleReverseWASD : LevelRuleBase
     {
+        [SerializeField] private GameObject _normalMoveGUI;
+        [SerializeField] private GameObject _reverseMoveGUI;
+        [SerializeField] private GameObject _normalJumpCrouchGUI;
+        [SerializeField] private GameObject _reverseJumpCrouchGUI;
+
         public override void OnEnter()
         {
             base.OnEnter();
 
-            _playerStateMachine.Paramaters.ReverseWASD = true;
+            //显示反向WASD的GUI
+            _normalMoveGUI.SetActive(false);
+            _normalJumpCrouchGUI.SetActive(false);
+
+            _reverseMoveGUI.SetActive(true);
+            _reverseJumpCrouchGUI.SetActive(false);
         }
 
         public override void OnExit()
         {
             base.OnExit();
 
-            _playerStateMachine.Paramaters.ReverseWASD = false;
+            //恢复
+            _normalMoveGUI.SetActive(true);
+            _normalJumpCrouchGUI.SetActive(true);
+
+            _reverseMoveGUI.SetActive(false);
+            _reverseJumpCrouchGUI.SetActive(false);
         }
     }
 }
